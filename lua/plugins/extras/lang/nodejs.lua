fe1ca217fc7e7a1f895e55a93c47b399e1c709fa
jerry <calljerryli@outlook.com>
2023-03-12 22:40:45 +0800

    nls.builtins.formatting.prettier

--- lua/plugins/extras/lang/nodejs.lua
+++ lua/plugins/extras/lang/nodejs.lua
@@ -35,10 +35,7 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(
-        opts.ensure_installed,
-        { "css-lsp", "eslint-lsp", "html-lsp", "stylelint-lsp", "typescript-language-server" }
-      )
+      vim.list_extend(opts.ensure_installed, { "css-lsp", "eslint-lsp", "html-lsp", "typescript-language-server" })
     end,
   },
 }
