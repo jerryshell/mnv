22a7509191680d05aaf95d64fc693c1250cef0c9
Jerry <calljerryli@outlook.com>
2025-03-18 21:14:45 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gdscript" },
+  callback = function()
+    vim.bo.sw = 4
+    vim.bo.sts = 4
+    vim.bo.ts = 4
+    vim.bo.expandtab = false
+    vim.bo.softtabstop = 4
+  end,
+})
