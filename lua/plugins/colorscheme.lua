1263367ea4ec3a6d25e5b90a037cf2afde02de0a
jerry <calljerryli@outlook.com>
2025-07-13 22:05:07 +0800

    remove term colors and color overrides from catppuccin options; set transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,14 +9,7 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
-      term_colors = true,
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
+      transparent_background = true,
     },
   },
 }
