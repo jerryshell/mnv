3666c4590ea5a11fda60e172652fad2a579601d4
Jerry <calljerryli@outlook.com>
2025-07-31 18:53:45 +0800

    update lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    },
+  },
+}
